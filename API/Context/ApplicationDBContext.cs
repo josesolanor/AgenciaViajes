1ef700455497d642dea71a4977af74ba5a7f7abb
Jose Solano R <josesolano@outlook.com>
2020-03-05 22:41:20 -0400

    Revert "merge"
    This reverts commit 0875b79039c984c5eaf25a78a69f4bb623b94dc6.

---
+++ API/Context/ApplicationDBContext.cs
@@ -0,0 +1,36 @@
+﻿using AgenciaViajes.Presentation.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgenciaViajes.Presentation.API.Context
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+
+
+        public DbSet<TravelPackage> TravelPackages { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<TravelPackage>(eb =>
+                {
+                    eb.HasKey(x => x.Id);
+                    eb.OwnsOne(x => x.TouristPlace);
+                });
+
+            modelBuilder.Entity<Booking>()
+                .HasOne(bc => bc.Client)
+                .WithMany(b => b.TravelPackages)
+                .HasForeignKey(bc => bc.IdClient);
+            modelBuilder.Entity<Booking>()
+                .HasOne(bc => bc.TravelPackage)
+                .WithMany(c => c.Clients)
+                .HasForeignKey(bc => bc.IdTravelPackage);
+        }
+    }
+}
---
+++ API/Entities/TravelPackage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AgenciaViajes.Presentation.API.Entities
+{
+    public class TravelPackage
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Type { get; set; }
+        public TouristPlace TouristPlace { get; set; }
+        public List<Booking> Clients { get; set; }
+    }
+}
